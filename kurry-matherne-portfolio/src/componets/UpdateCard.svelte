b9b765a7d0c7e80e747a09789eb80036f9d9eb31
AlexPHebert2000 <aphebert9994@gmail.com>
2024-07-23 20:01:21 -0500

    ADD update card component

---
+++ kurry-matherne-portfolio/src/componets/UpdateCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { Update } from "@prisma/client";
+  import dayjs from 'dayjs';
+
+  export let update : Update;
+</script>
+
+<div class="bg-slate-900 bor text-white rounded-md mb-3 p-2">
+  <div class="flex justify-between">
+    <h1 class="font-semibold text-lg">{update.title}</h1>
+    <h1>{dayjs(update.createdAt).format("DD/MM/YYYY")}</h1>
+  </div>
+  <p>{update.body}</p>
+</div>
